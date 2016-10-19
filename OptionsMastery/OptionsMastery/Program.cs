daa9b754db18e61575fef6d73dc7b70b2acf1acc
Gabrieltay <Gabrieltaykl@gmail.com>
2016-10-19 23:41:25 +0800

    Init

---
+++ OptionsMastery/OptionsMastery/Program.cs
@@ -0,0 +1,23 @@
+﻿using OptionsMastery.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptionsMastery
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault( false );
+            Application.Run( new Mainform() );
+        }
+    }
+}
